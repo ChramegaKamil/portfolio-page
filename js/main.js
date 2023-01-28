ea1874a0f1f3d9be7b7d768d6ca2a51a93f8ea96
Chramęga Kamil <116751886+ChramegaKamil@users.noreply.github.com>
2023-01-28 22:44:48 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const header = document.querySelector('header');
+
+const stickHeader = () => {
+  const scroll = window.scrollY;
+
+  if (scroll > 0) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
+};
+
+window.addEventListener('scroll', stickHeader);
+
+const menu = document.querySelector('.menu');
+const burgerBtn = document.querySelector('.burger');
+
+burgerBtn.addEventListener('click', () => {
+  burgerBtn.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+const menuLinks = document.querySelectorAll('.menu-link');
+
+menuLinks.forEach((link) =>
+  link.addEventListener('click', (e) => {
+    const key = e.target.dataset.key;
+
+    const section =
+      document.querySelector(`.${key}`).getBoundingClientRect().top +
+      window.pageYOffset -
+      80;
+
+    menu.classList.remove('active');
+    burgerBtn.classList.remove('active');
+
+    window.scrollTo({ top: section, behavior: 'smooth' });
+  })
+);
+
+
+
+
+
+
+
+
+const spanYear = document.querySelector('.footer-year')
+
+const handleCurrentYear = () => {
+    const year = new Date().getFullYear()
+    spanYear.textContent = year}
+handleCurrentYear()
